d99b804eaffab01d6c097c0eba2bd9eb5508367a
Jairo Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2024-10-03 16:49:58 -0600

    parametros por url

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -19,6 +19,7 @@ const appRoutes: Routes = [
     {path: 'formulario', component: FormularioComponent},
     {path: 'peliculas', component: PeliculasComponent},
     {path: 'pagina-de-pruebas', component: PaginaComponent},
+    {path: 'pagina-de-pruebas/:nombre/:apellido', component: PaginaComponent},
     {path: '**', component: ErrorComponent}
 ]
 
--- src/app/components/pagina/pagina.component.ts
+++ src/app/components/pagina/pagina.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-pagina',
   templateUrl: './pagina.component.html',
   styleUrl: './pagina.component.css'
 })
-export class PaginaComponent {
+export class PaginaComponent  implements OnInit{
+
+  public nombre?: string;
+  public apellido?: string;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ){}
+
+  ngOnInit() {
+      this._route.params.subscribe((params: Params) => {
+        this.nombre = params['nombre'];
+        this.apellido = params['apellido'];
+      });
+  }
 
 }
